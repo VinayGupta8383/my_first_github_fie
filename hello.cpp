6411faa7ee41007d665ccfbfc3cca990b856f5ac
VinayGupta8383 <134718534+VinayGupta8383@users.noreply.github.com>
2023-05-26 07:57:43 -0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+//welcome vinay
+
+int a;
+int b;
+cout<<"enter first number: "<<endl;
+cin>>a;
+cout<<"enter second number: "<<endl;
+cin>>b;
+cout<<"sum of num1 and num2 is : "<<a+b<<endl;
+
+
+return 0;
+}
